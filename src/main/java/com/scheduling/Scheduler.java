e473ba64f79b707a42667065d5dd21e4cb145b00
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-06 20:14:02 -0600

    the @Schedule annotation is used to trigger the cheduler for a specific time period

---
+++ src/main/java/com/scheduling/Scheduler.java
@@ -0,0 +1,20 @@
+package com.scheduling;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class Scheduler {
+
+  @Scheduled(cron = "0 * 19 * * ?")
+  public void cronJobScheduler(){
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    Date now = new Date();
+    String stringDate = sdf.format(now);
+    System.out.println("Java cron job expressions %s%n".formatted(stringDate));
+  }
+
+}
