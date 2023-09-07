16d5eae83ae9491558700531eb3acd8132d93610
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-06 18:37:33 -0600

    enable the scheduler for this application

--- src/main/java/com/scheduling/SchedulingApplication.java
+++ src/main/java/com/scheduling/SchedulingApplication.java
@@ -2,12 +2,14 @@ package com.scheduling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchedulingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchedulingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SchedulingApplication.class, args);
+  }
 
 }
